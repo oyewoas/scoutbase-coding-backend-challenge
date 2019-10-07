5e5eef6736793f8c437557646e0e440b365aec86
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-10-07 02:54:10 +0100

    created the models for the database

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv'
+import { config } from 'dotenv'
 
-dotenv.config()
+config()
 
 module.exports = {
   development: {
---
+++ back-end/models/users.js
@@ -0,0 +1,25 @@
+
+const users = (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [7, 42],
+      },
+    }
+  });
+  
+    return Users;
+};
+
+export default users;
